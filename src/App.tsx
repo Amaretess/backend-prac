dc928dafd33d2bbb2281b19e134392f4fb109c82
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-01-23 21:46:56 -0800

    error handling

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,21 @@ interface User {
 const App = () => {
 
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     axios.get<User[]>("https://jsonplaceholder.typicode.com/users")
       .then(response => setUsers(response.data))
+      .catch((error) => setError(error.message))
   }, [])
 
   return (
-    <ul>
-      {users.map(user => <li key={user.id} >{user.name}</li>)}
-    </ul>
+    <>
+      {error && <p className="text-danger" >{error}</p>}
+      <ul>
+        {users.map(user => <li key={user.id} >{user.name}</li>)}
+      </ul>
+    </>
   )
 }
 
