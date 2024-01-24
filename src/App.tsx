944f4102b3015b10b56d6baeeade514409096c17
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-01-23 20:59:29 -0800

    http requests

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 const App = () => {
 
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios.get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then(response => setUsers(response.data))
+  }, [])
 
   return (
-    <div>
-
-    </div>
+    <ul>
+      {users.map(user => <li key={user.id} >{user.name}</li>)}
+    </ul>
   )
 }
 
