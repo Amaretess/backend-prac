81e427b573c1e1c28e52cb5ea2884c6b82b20ecc
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-01-25 23:35:02 -0800

    updating the server

--- src/App.tsx
+++ src/App.tsx
@@ -37,11 +37,14 @@ const App = () => {
 
 
   const deleteUser = (user: User) => {
+    const originalUsers = [...users]
+
     setUsers(users.filter(u => u.id !== user.id))
 
     axios.delete(`https://jsonplaceholder.typicode.com/users/${user.id}`)
       .catch(err => {
-
+        setError(err.message);
+        setUsers(originalUsers)
       })
   }
 
